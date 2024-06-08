da8fed424a3b4250c7614f7384bcc3bbd5e257db
ad <codemail.ad@gmail.com>
2024-06-08 18:26:09 +0200

    Make set_end_of_day() static

--- app/models.py
+++ app/models.py
@@ -6,11 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-def set_end_of_day():
-    current_date = datetime.now()
-    return current_date.replace(hour=23, minute=59, second=59)
-
-
 class TodoItem(Base):
     __tablename__ = 'todo_items'
 
@@ -19,6 +14,12 @@ class TodoItem(Base):
     description = Column(String)
     priority = Column(Integer, default=0, nullable=False)
     done = Column(Boolean, default=False)
+
+    @staticmethod
+    def set_end_of_day():
+        current_date = datetime.now()
+        return current_date.replace(hour=23, minute=59, second=59)
+
     due_date = Column(Date, default=set_end_of_day)
 
     def __repr__(self):
