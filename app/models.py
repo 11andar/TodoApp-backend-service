fc659a7c669a223594c6b1f3525b9955920b372a
ad <codemail.ad@gmail.com>
2024-06-08 18:20:52 +0200

    Set default due date to the end of the day

--- app/models.py
+++ app/models.py
@@ -1,9 +1,14 @@
-from datetime import date
+from datetime import datetime
 from sqlalchemy import Column, Integer, String, Date, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
+
+
+def set_end_of_day():
+    current_date = datetime.now()
+    return current_date.replace(hour=23, minute=59, second=59)
 
 
 class TodoItem(Base):
@@ -14,7 +19,7 @@ class TodoItem(Base):
     description = Column(String)
     priority = Column(Integer, default=0, nullable=False)
     done = Column(Boolean, default=False)
-    due_date = Column(Date, default=date.today)
+    due_date = Column(Date, default=set_end_of_day)
 
     def __repr__(self):
         return f"""Title: {self.title}, Due date: {self.due_date}"""
