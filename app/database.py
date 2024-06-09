9f78ea7be68f93321e6d1f190bf09b7e32662392
ad <codemail.ad@gmail.com>
2024-06-09 12:56:35 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
 
 engine = create_engine(DATABASE_URL)
 LocalSession = sessionmaker(bind=engine)
