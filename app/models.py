3b5ee12d8b2bdc275d97b9c2fe802a9d6db1d3b0
ad <codemail.ad@gmail.com>
2024-06-10 11:55:29 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,11 @@ from sqlalchemy import Column, Integer, String, Date, Boolean
 from database import Base
 
 
+def set_end_of_current_day() -> datetime:
+    current_day = datetime.utcnow()
+    return current_day.replace(hour=23, minute=59, second=59)
+
+
 class TodoItem(Base):
     __tablename__ = 'todo_items'
 
@@ -11,13 +16,7 @@ class TodoItem(Base):
     description = Column(String, default=None)
     priority = Column(Integer, default=0, nullable=False)
     done = Column(Boolean, default=False)
-
-    @staticmethod
-    def set_end_of_day():
-        current_date = datetime.now()
-        return current_date.replace(hour=23, minute=59, second=59).date()
-
-    due_date = Column(Date, default=set_end_of_day)
+    due_date = Column(Date, default=set_end_of_current_day)
 
     def __repr__(self):
         return f"""Title: {self.title}, Due date: {self.due_date}"""
