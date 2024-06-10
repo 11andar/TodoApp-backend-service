e8de9f20192e78ab02713f6ab68ace62b037851f
ad <codemail.ad@gmail.com>
2024-06-10 12:58:06 +0200

    Read single todo or multiple todos

--- app/crud.py
+++ app/crud.py
@@ -9,3 +9,11 @@ def create_todo(db: Session, todo: TodoCreate) -> TodoItem:
     db.commit()
     db.refresh(db_item)
     return db_item
+
+
+def get_todo_item(db: Session, id: int) -> TodoItem:
+    return db.query(TodoItem).filter(TodoItem.id == id).first()
+
+
+def get_todos(db: Session, skip: int = 0, limit: int = 10) -> list[TodoItem]:
+    return db.query(TodoItem).offset(skip).limit(limit).all()
