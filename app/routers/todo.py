aca2682dd07fa046c1bfcb6c9e6f43c3c4325675
ad <codemail.ad@gmail.com>
2024-06-10 15:33:10 +0200

    Implement update operation

--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.database import get_db
@@ -20,3 +20,11 @@ async def get_todo_item(todo_id: int, db: Session = Depends(get_db)):
 @router.get("/todos/", response_model=List[schemas.TodoRead])
 async def get_todos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_todos(db=db, skip=skip, limit=limit)
+
+
+@router.put("/todos/{todo_id}", response_model=schemas.TodoRead)
+async def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(get_db)):
+    db_item = crud.update_todo(db=db, todo_id=todo_id, todo=todo)
+    if db_item:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_item
