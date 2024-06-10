974299bc9cf78cbbb7152b8f7c717f0937e00da5
ad <codemail.ad@gmail.com>
2024-06-10 13:00:55 +0200

    Update todo

--- app/crud.py
+++ app/crud.py
@@ -17,3 +17,15 @@ def get_todo_item(db: Session, id: int) -> TodoItem:
 
 def get_todos(db: Session, skip: int = 0, limit: int = 10) -> list[TodoItem]:
     return db.query(TodoItem).offset(skip).limit(limit).all()
+
+
+def update_todo(db: Session, id: int, todo: TodoUpdate) -> TodoItem:
+    db_item = db.query(TodoItem).filter(TodoItem.id == id).first()
+    if db_item:
+        for key, value in todo.dict(exclude_unset=True).items():
+            setattr(db_item, key, value)
+        db.commit()
+        db.refresh(db_item)
+    return db_item
+
+
