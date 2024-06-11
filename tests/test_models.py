99e7891c41c7b00dcc43ccf1b958ce4ebbffc97e
ad <codemail.ad@gmail.com>
2024-06-11 09:28:44 +0200

    Test custom item

--- tests/test_models.py
+++ tests/test_models.py
@@ -39,3 +39,21 @@ def test_todo_item_default(db_session):
     assert todo_item.due_date.minute == 59
     assert todo_item.due_date.second == 59
     assert todo_item.due_date.microsecond == 999999
+
+
+def test_todo_item_custom(db_session):
+    custom_title = "Custom Title"
+    custom_description = "Custom Description"
+    custom_priority = 1
+    custom_done = True
+    custom_due_date = datetime.utcnow().replace(hour=21, minute=37, second=00, microsecond=000000)
+    test_item = TodoItem(title=custom_title, description=custom_description,
+                         priority=custom_priority, done=custom_done, due_date=custom_due_date)
+    db_session.add(test_item)
+    db_session.commit()
+
+    assert test_item.title == custom_title
+    assert test_item.description == custom_description
+    assert test_item.priority == custom_priority
+    assert test_item.done == custom_done
+    assert test_item.due_date == custom_due_date
