5b0369ad850666c60ed878008a03c6d249d98e4f
ad <codemail.ad@gmail.com>
2024-06-11 09:12:09 +0200

    Test todo_item initialization

--- tests/test_models.py
+++ tests/test_models.py
@@ -25,3 +25,17 @@ def test_set_end_of_current_day(db_session):
     end_of_current_day = current_day.replace(hour=23, minute=59, second=59, microsecond=999999)
     date_returned = set_end_of_current_day()
     assert date_returned == end_of_current_day
+
+
+def test_todo_item_init(db_session):
+    todo_item = TodoItem(title="test")
+    db_session.add(todo_item)
+    db_session.commit()
+    assert todo_item.title == "test"
+    assert todo_item.description is None
+    assert todo_item.priority == 0
+    assert todo_item.done == False
+    assert todo_item.due_date.hour == 23
+    assert todo_item.due_date.minute == 59
+    assert todo_item.due_date.second == 59
+    assert todo_item.due_date.microsecond == 999999
