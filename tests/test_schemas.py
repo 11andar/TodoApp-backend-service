bb4bf812708911bd80e5d9f0db264e9540e56a93
ad <codemail.ad@gmail.com>
2024-06-11 11:23:29 +0200

    Test due_date absent

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -8,3 +8,9 @@ def test_todo_base_due_date_none():
     data = {"due_date": None}
     test_base = TodoBase(**data)
     assert test_base.due_date == set_end_of_current_day()
+
+
+def test_todo_base_due_date_absent():
+    data = {}
+    test_base = TodoBase(**data)
+    assert test_base.due_date == set_end_of_current_day()
